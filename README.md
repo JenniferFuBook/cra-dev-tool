abe41d5bd39eaa057ff3475d83d77a7ef88ce30d
Jennifer Fu <jennifer.fu@dominodatalab.com>
2023-08-16 21:49:06 -0700

    Update files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a Chrome browser extension that serves the UI Vite app from your local machine 
+This is a Chrome browser extension that serves the UI of Create React App from your local machine 
 while you're browsing a cloud deployment.
 
 To get started, go to chrome://extensions on your Chrome browser. Turn on the Developer Mode switch 
@@ -6,8 +6,8 @@ located in the top right corner.
 
 Click on Load unpacked and select the directory of this repository.
 
-Execute following commands at root directory: 
-* yarn dev
-* yarn build -- --watch
+Follow these two steps to start:
 
-Go to the cloud deployment URL to debug the code from your local machine.
+* Start the Web application from your local machine.
+
+* Go to the cloud deployment URL to debug the code from your local machine.
--- background.js
+++ background.js
@@ -1,37 +1,110 @@
+const localFEServer = 'http://localhost:8080';
+const localWPServer = 'http://localhost:3000';
+
 const RULES = [
   {
-    "id" : 1,
-    "action" : { 
-      "type" : "redirect",
-      "redirect": {
-        "url": "http://127.0.0.1:5173/dist/assets/index.js",
+    id: 1,
+    action: {
+      type: 'redirect',
+      redirect: {
+        url: `${localFEServer}/static/js/bundle.js`,
       },
     },
-    "condition" : {
-      "urlFilter": 'index-*.js',
-      "resourceTypes" : ["script"]
-    }
+    condition: {
+      urlFilter: '/static/js/main.*.js',
+      resourceTypes: ['script'],
+    },
   },
   {
-    "id" : 2,
-    "action" : { 
-      "type" : "redirect",
-      "redirect": {
-        "url": "http://127.0.0.1:5173/dist/assets/index.css",
+    id: 2,
+    action: {
+      type: 'block',
+    },
+    condition: {
+      urlFilter: '/static/js/main.*.css',
+      resourceTypes: ['stylesheet'],
+    },
+  },
+  {
+    id: 3,
+    action: {
+      type: 'redirect',
+      redirect: {
+        transform: {
+          host: `${localFEServer}/static/js`,
+        },
       },
     },
-    "condition" : {
-      "urlFilter": 'index-*.css',
-      "resourceTypes" : ["stylesheet"]
-    }
-  }
+    condition: {
+      regexFilter: 'chunk',
+      resourceTypes: ['script'],
+    },
+  },
+  {
+    id: 4,
+    action: {
+      type: 'redirect',
+      redirect: {
+        transform: {
+          host: localWPServer,
+        },
+      },
+    },
+    condition: {
+      regexFilter: '[a-zA-Z0-9]+\\.manifest\\.js',
+      resourceTypes: ['script'],
+    },
+  },
+  {
+    id: 5,
+    action: {
+      type: 'redirect',
+      redirect: {
+        transform: {
+          host: localWPServer,
+        },
+      },
+    },
+    condition: {
+      regexFilter: '[a-zA-Z0-9]+\\.vendor\\.js',
+      resourceTypes: ['script'],
+    },
+  },
+  {
+    id: 6,
+    action: {
+      type: 'redirect',
+      redirect: {
+        transform: {
+          host: localWPServer,
+        },
+      },
+    },
+    condition: {
+      regexFilter: '[a-zA-Z0-9]+\\.ReactComponents\\.js',
+      resourceTypes: ['script'],
+    },
+  },
+  {
+    id: 7,
+    action: {
+      type: 'redirect',
+      redirect: {
+        transform: {
+          host: localWPServer,
+        },
+      },
+    },
+    condition: {
+      regexFilter: '[a-zA-Z0-9]+\\.ReactComponents\\.css',
+      resourceTypes: ['stylesheet'],
+    },
+  },
 ];
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeNetRequest.updateDynamicRules(
-    {
-      "removeRuleIds": RULES.map(r => r.id),
-      "addRules": RULES,
-    },
-  );
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: RULES.map((r) => r.id),
+    addRules: RULES,
+  });
 });
